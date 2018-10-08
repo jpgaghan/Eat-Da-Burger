6048c17682d4a8b9e57f1fefd0e6790ce337947d
Jonathan Gaghan <jpgaghan@gmail.com>
2018-10-08 00:23:21 -0400

    env

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,21 @@
 CREATE DATABASE burgers_db;
 USE burgers_db;
 CREATE TABLE burgers(
-    id int NOT NULL AUTO_INCREMENT,
+    id INT AUTO_INCREMENT NOT NULL,
 	burger_name varchar(255) NOT NULL,
 	devoured boolean DEFAULT false,
-    PRIMARY KEY (id)
+    createdAt TIMESTAMP NOT NULL,
+    PRIMARY KEY(id)
 );
+
+USE foncor7yzmqi3qrf;
+DROP TABLE burgers;
+CREATE TABLE burgers(
+    id INT AUTO_INCREMENT NOT NULL,
+	burger_name varchar(255) NOT NULL,
+	devoured boolean DEFAULT false,
+    createdAt TIMESTAMP NOT NULL,
+    PRIMARY KEY(id)
+);
+ALTER TABLE burgers CHANGE `createdAt` `createdAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
